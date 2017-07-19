d1acd5ee5f81c3a2b6108e154813e4a183b6f73b
lsiegle <lsiegle@twilio.com>
2017-07-19 14:14:25 -0700

    ugh git

--- web/chrome-dev-ed-extension/main.js
+++ web/chrome-dev-ed-extension/main.js
@@ -1,6 +1,6 @@
 var links = document.getElementsByTagName("a");
 
 for (var i = 0; i < links.length; i++) {
-	//links[i].href = "https://goo.gl/SsAhv"
+	//links[i].href = "https://goo.gl/SsAhv" (rick roll link)
 	links[i].href = "https://www.twilio.com/console"
 }
